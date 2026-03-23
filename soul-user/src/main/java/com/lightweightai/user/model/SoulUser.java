dea93e32d19607de5223b9901b7a8d89e0778547
helloworldhelloworld <xiajie198912@gmail.com>
2026-03-23 19:18:35 +0800

    add backend register/login with password hashing and session tokens

--- soul-user/src/main/java/com/lightweightai/user/model/SoulUser.java
+++ soul-user/src/main/java/com/lightweightai/user/model/SoulUser.java
@@ -1,10 +1,15 @@
 package com.lightweightai.user.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 /**
  * A SoulHarbor user (anonymous or registered).
  */
 public class SoulUser {
     private String id;
+    private String username;
+    @JsonIgnore
+    private String passwordHash;
     private String openid;
     private String nickname;
     private String memberLevel;
@@ -13,8 +18,10 @@ public class SoulUser {
 
     public SoulUser() {}
 
-    public SoulUser(String id, String openid, String nickname, String memberLevel, long createdAt, long lastActive) {
+    public SoulUser(String id, String username, String passwordHash, String openid, String nickname, String memberLevel, long createdAt, long lastActive) {
         this.id = id;
+        this.username = username;
+        this.passwordHash = passwordHash;
         this.openid = openid;
         this.nickname = nickname;
         this.memberLevel = memberLevel;
@@ -24,6 +31,10 @@ public class SoulUser {
 
     public String getId() { return id; }
     public void setId(String id) { this.id = id; }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public String getPasswordHash() { return passwordHash; }
+    public void setPasswordHash(String passwordHash) { this.passwordHash = passwordHash; }
     public String getOpenid() { return openid; }
     public void setOpenid(String openid) { this.openid = openid; }
     public String getNickname() { return nickname; }
